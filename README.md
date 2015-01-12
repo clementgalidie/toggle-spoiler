0087ef2f5babd77598eb9604e5f3de908cd1d41d
Clement Galidie <galidie.clement@gmail.com>
2015-01-12 22:30:33 +0100

    Add readme, assets and main html file

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Description
 
-This is a very basic **JavaScript** *Vanilla* script whose the purpose is to toggle a spoiler by clicking on a `<button type="button">button</button>` element.
+This is a very basic **JavaScript** *Vanilla* script whose the purpose is to toggle a spoiler by clicking on a `<button type="button"></button>` element.
 
 ## Interoperability
--- assets/js/spoiler.js
+++ assets/js/spoiler.js
@@ -1,12 +1,14 @@
 (function () {
 	"use strict";
-	
+
 	var buttonShowSpoiler = document.querySelectorAll(".spoiler-button");
+
 	// Function to show spoilers.
 	var showSpoiler = function showSpoiler(spoiler) {
 		this.nextSibling.nextSibling.classList.remove("spoiler-content--hidden");
 		this.classList.remove("spoiler-button--show");
 	}
+	
 	// When clicking on "spoiler button", call showSpoiler().
 	for (var i = 0; i < buttonShowSpoiler.length; i++) {
 		buttonShowSpoiler[i].addEventListener("click", showSpoiler);
